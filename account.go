cb1e7d0079db814d1496928460da15ac923d9010
Xia <xiajinxue@sharkgulf.com>
2023-03-10 16:52:30 +0800

    init

---
+++ account.go
@@ -0,0 +1,5 @@
+package model
+
+const (
+	AccountToken = "account:token:%s"
+)
--- app_model.go
+++
@@ -1,9 +0,0 @@
-package model
-
-import "gorm.io/gorm"
-
-type App struct {
-	Name   string `gorm:"default:'app'"`
-	Status bool   `gorm:"default:true"`
-	gorm.Model
-}
